11801ea2d505775ff936dc8127b8704bffaf98ca
Abel-huanreng <72290475+Abel-huanreng@users.noreply.github.com>
2022-07-29 09:48:12 +0800

    print

--- web/src/main/java/com/example/web/Hejing.java
+++ web/src/main/java/com/example/web/Hejing.java
@@ -3,6 +3,8 @@ package com.example.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.PrintStream;
+
 /**
  * @author hejing
  * @date 2022/7/28:17:08
@@ -11,7 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hejing {
     @GetMapping("/myIndex")
     public String index() {
-        System.out.println("hello.springboot的第一个controller");
+        PrintStream myStream = new PrintStream(System.out) {
+            @Override
+            public void println(String x) {
+                super.println(System.currentTimeMillis() + ": " + x);
+            }
+        };
+        System.setOut(myStream);
+        System.out.println("\033[40;31;4m" + "我滴个颜什" + "\033[0m");
+        System.out.println("Hello World!");
         return "index1";
     }
 }
